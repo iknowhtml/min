6b00764ab4a3a3c44c9cb8b3b02b3fa12bf55e1a
Aki Gao <xag9bb@virginia.edu>
2015-10-28 02:29:25 -0400

    finished demo

--- demo.py
+++ demo.py
@@ -10,6 +10,15 @@ import os
 import time
 import struct
 
+#import opencv
+import cv2
+
+#sets up face cascade
+cascPath = 'face_cascade.xml'
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(-1)
+
 #imports for audio
 import pyaudio
 import audioop
@@ -28,8 +37,11 @@ def Main():
 	initialize()
 
 	while True:
+        detectFace()
+        stand()
 		detectSound()
 		wave()
+        sitDown()
 
 def initialize():
         try:
@@ -44,13 +56,61 @@ def initialize():
                 api.ServoShutdown()
                 sys.exit()
 
+def stand():
+    api.PlayAction(8)
+    print("Min Stood up!")
+
+def sit():
+    api.PlayAction(15)
+    print("Min Sat Down!")
+
 def wave():
 	api.PlayAction(25)
         print('Min Waved!')
 
+def detectFace():
+    val = 0
+
+    while True:
+        # Capture frame-by-frame
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        
+        if val % 10 == 0:   
+            print("analyzing frame")
+            faces = faceCascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=5,
+                minSize=(30, 30),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+        
+        print("frame analyzed!")
+            # Draw a rectangle around the faces
+            
+        #for (x, y, w, h) in faces:
+                #cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            if len(faces):
+                print("face detected")
+                # When everything is done, release the capture
+                #video_capture.release()
+                #cv2.destroyAllWindows()
+                break
+            
+            # Display the resulting frame
+            #cv2.imshow('Video', frame)
+
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        val+= 1
+
 def detectSound():
 	#initialize sound pyaudio
-        p = pyaudio.PyAudio()
+    p = pyaudio.PyAudio()
 	stream = p.open(format=FORMAT,
                 channels=CHANNELS,
                 rate=RATE,
@@ -60,27 +120,31 @@ def detectSound():
 	print("detecting sound...")
 
 	while True:
-    		try:
-        		data = stream.read(CHUNK)
-    		except IOError as ex:
-        		stream = p.open(format=FORMAT,
-                	channels=CHANNELS,
-                	rate=RATE,
-                	input=True,
-                	input_device_index = 2)
+		try:
+    		data = stream.read(CHUNK)
+		except IOError as ex:
+    		stream = p.open(format=FORMAT,
+            	channels=CHANNELS,
+            	rate=RATE,
+            	input=True,
+            	input_device_index = 2)
 
-        		data = stream.read(CHUNK)
+    		data = stream.read(CHUNK)
 
-    		rms = audioop.rms(data, 2)
-    		print(rms)
-    		if(rms>THRESHOLD):
-			print("*sound detected!")
+		rms = audioop.rms(data, 2)
+        print(rms)
 
-			stream.stop_stream()
-			stream.close()
-			p.terminate()
-			
-			break
+		if(rms>THRESHOLD):
+		  print("*sound detected!")
+
+		  stream.stop_stream()
+		  stream.close()
+		  p.terminate()
+		
+		  break
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 if __name__ == "__main__": 
   Main()
