c7005d3bebdc030d66e21e747166a73930243012
Aki Gao <xag9bb@virginia.edu>
2015-10-28 04:00:29 -0400

    modified test to test if sys.append causing issues

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import sys
 
 #appends api directory path to sys path
 sys.path.append("/home/pi/HROS1-Framework/Linux/project/Human_Robots_Interaction_Fall15")
-import api
+#import api
 
 import ctypes
 import os
@@ -23,19 +23,6 @@ def Main():
     initialize()
     while True:
         detectFace() 
-
-def initialize():
-    try:
-        if api.Initialize():
-            print("Initalized")
-        else:
-            print("Intialization failed")
-    except (KeyboardInterrupt):
-        api.ServoShutdown()
-        sys.exit()
-    except():
-            api.ServoShutdown()
-            sys.exit()
 
 def detectFace():
     val = 0
